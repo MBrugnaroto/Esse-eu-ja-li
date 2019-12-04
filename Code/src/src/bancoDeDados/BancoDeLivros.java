280a6c447342e63b19bbd234b74ae039fb4707fb
Mateus Brugnaroto <brugnaroto@gmail.com>
2019-12-04 17:44:15 +0100

    FEAT. Implementação do acesso ao banco de livros da biblioteca (get livro e getListaDeLivros).

---
+++ Code/src/src/bancoDeDados/BancoDeLivros.java
@@ -0,0 +1,38 @@
+package src.bancoDeDados;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BancoDeLivros extends TratamentoDeArquivos {
+	private List<String[]> _isDadosDoArquivo;
+	
+	public BancoDeLivros () throws FileNotFoundException, IOException {
+		_isDadosDoArquivo = lendoOArquivo("Registro de Livros");
+	}
+
+	public String[] getLivro() {
+		if (_isDadosDoArquivo == null) {
+			return null;
+		}
+		return _isDadosDoArquivo.get(0);
+	}
+
+	public List<String> getListaDeLivros() {
+		List<String> listaLivros = new ArrayList<>();
+		
+		if (_isDadosDoArquivo == null) {
+			listaLivros.add("Não há livros na biblioteca");
+			return listaLivros;
+		}
+		
+		for (int i = 0; i < _isDadosDoArquivo.size(); i++) {
+			listaLivros.add(_isDadosDoArquivo.get(i)[0]);
+		}
+		return listaLivros;
+	}
+	
+	
+	
+}
